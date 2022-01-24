5f0f7069a84800d08669cfff78bec9d4068940fa
Alex B. Jose <857437+abjose@users.noreply.github.com>
2022-01-24 13:42:46 -0800

    Add some SB graph models

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Choice, Question
+from .models import Choice, Question, Topic, Relationship
 
 
 class ChoiceInline(admin.TabularInline):
@@ -18,4 +18,7 @@ class QuestionAdmin(admin.ModelAdmin):
     list_filter = ['pub_date']
     search_fields = ['question_text']
 
-admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Question, QuestionAdmin)
+
+admin.site.register(Topic)
+admin.site.register(Relationship)
--- polls/models.py
+++ polls/models.py
@@ -28,3 +28,48 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+
+class Topic(models.Model):
+    topic_title = models.CharField(max_length=200)
+
+    # How does this compare to having explicit Edges?
+    # parent_topics = models.ManyToManyField(
+    #     to='self', symmetrical=False, blank=True)
+    # prereq_topics = models.ManyToManyField(
+    #     to='self', blank=True)
+    # https://stackoverflow.com/questions/46268059/django-many-to-many-recursive-relationship
+
+    # resources = models.ManyToManyField(Resource)
+    # ehh, maybe just keep as foreign key - otherwise hard to keep track of topic-specific date for a resource
+
+    def __str__(self):
+        return self.topic_title
+
+
+# TODO: investigate performance of this vs. ManyToMany version.
+class Relationship(models.Model):
+    source_topic = models.ForeignKey(Topic, related_name="successor_relations", on_delete=models.CASCADE)
+    target_topic = models.ForeignKey(Topic, related_name="predecessor_relations", on_delete=models.CASCADE)
+
+    # Should these be backwards?
+    class RelationType(models.IntegerChoices):
+        CHILD_OF = 1
+        PREREQ_OF = 2
+    relation_type = models.IntegerField(choices=RelationType.choices)
+
+    # one big advantage of explicit Relationship model - can easily have stuff like weights!
+
+    def __str(self):
+        return f"{self.source_topic} -> {self.target_topic}"
+
+
+# Want to be able to reference many topics?
+# Use Relationship instead?
+class Resource(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    resource_title = models.CharField(max_length=200)
+    resource_link = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.resource_title
